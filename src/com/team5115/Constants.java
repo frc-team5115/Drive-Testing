69a0d824e32b14727f4cf710dce28b806f0b08b5
cruzsbrian <cruz.s.brian@gmail.com>
2016-07-16 13:41:16 -0400

    Rename Const.java to Constants.java

--- src/com/team5115/Constants.java
+++ src/com/team5115/Constants.java
@@ -1,10 +1,11 @@
-public class Const {
+public class Constants {
 	
 	// Buttons and Axes
 	public static final int BUTTON_QUICK_TURN = 9;
 	
 	// Driving
-	public static final double NEG_INERTIA_SENSITIVITY = 1.0;
+	public static final double NEG_INERTIA_TURN = 1.0;
+	public static final double NEG_INERTIA_SPEED = 1.0;
 	public static final double JOYSTICK_DEADBAND = 0.01;
 	
 }
