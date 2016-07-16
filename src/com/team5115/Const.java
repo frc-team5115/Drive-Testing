29f352d0c0f18844d5b40d6cd67760fa40396c9d
cruzsbrian <cruz.s.brian@gmail.com>
2016-07-16 12:06:21 -0400

    Create constants file

---
+++ src/com/team5115/Const.java
@@ -0,0 +1,10 @@
+public class Const {
+	
+	// Buttons and Axes
+	public static final int BUTTON_QUICK_TURN = 9;
+	
+	// Driving
+	public static final double NEG_INERTIA_SENSITIVITY = 1.0;
+	public static final double JOYSTICK_DEADBAND = 0.01;
+	
+}
